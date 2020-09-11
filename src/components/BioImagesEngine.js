f912eb4a3d5252997677917e44cd738244927229
Gerhard Weis <g.weis@uq.edu.au>
2020-09-12 07:45:49 +1000

    bootstrap: cols within rows within a container (somewhere)

--- src/components/BioImagesEngine.js
+++ src/components/BioImagesEngine.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Col } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 import Favourite from "./bio-favourites/Favourite";
 import BioFacetsEngine from "./bio-image-facets/BioFacetsEngine";
 import BioMapEngine from "./bio-image-map/BioMapEngine";
@@ -20,27 +20,30 @@ const BioImagesEngine = () => {
   }, [dispatch]);
 
   return (
-    <div className="main-content d-flex">
-      {/* Filter SideBar */}
-      <Col xs={12} md={3} className="filter-bar">
-        <FilterHeader />
-        <BioFacetsEngine />
-        <Favourite />
-      </Col>
-      {/* TODO: the scroll-images class should probably be moved as separate
-          div into SearchEngine, just simple Col here */}
-      <Col md={9} className="scroll-images">
-        {/* Leaflet Map or Photo Gallery */}
-        {
-          searchMode === "Map"
-            ? (
-              <BioMapEngine />
-            ) : (
-              <SearchEngine />
-            )
-        }
-      </Col>
-    </div>
+    <Container fluid className="main-content">
+      <Row>
+        {/* <div className="main-content d-flex"> */}
+        {/* Filter SideBar */}
+        <Col xs={12} md={3} className="filter-bar">
+          <FilterHeader />
+          <BioFacetsEngine />
+          <Favourite />
+        </Col>
+        {/* TODO: the scroll-images class should probably be moved as separate
+            div into SearchEngine, just simple Col here */}
+        <Col md={9} className="scroll-images">
+          {/* Leaflet Map or Photo Gallery */}
+          {
+            searchMode === "Map"
+              ? (
+                <BioMapEngine />
+              ) : (
+                <SearchEngine />
+              )
+          }
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
