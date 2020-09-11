52fa862c9dbd87306110c6f17a2c4a48d79614ab
Gerhard Weis <g.weis@uq.edu.au>
2020-09-11 14:33:42 +1000

    support global menu configuration through configuration file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { TopBar, Footer, AppHeader } from "tern-react";
+import { TopBar, Footer, AppHeader, getTernMenu } from "tern-react";
 
+import { CONFIG } from "./config";
 import BioImagesAppHeader from "./components/headers/BioImagesAppHeader";
 import BioImagesEngine from "./components/BioImagesEngine";
 
 function App() {
   return (
     <Router>
-      <TopBar />
+      <TopBar menuConfig={getTernMenu(CONFIG.MENU)} />
       <AppHeader fluid>
         <BioImagesAppHeader />
       </AppHeader>
       <BioImagesEngine />
-      <Footer />
+      <Footer about={getTernMenu(CONFIG.MENU).about} />
     </Router>
   );
 }
--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 export const CONFIG = {
   API_BASE_URL: "https://bioimages-test.tern.org.au/api/v1.0/",
   LOGIN_URL: "https://bioimages-test.tern.org.au/login",
+  MENU: {
+    env: "test",
+  },
 };
