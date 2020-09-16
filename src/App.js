5e129d7ba5d969bdda479100ad63366ee876e505
Gerhard Weis <g.weis@uq.edu.au>
2020-09-17 08:30:54 +1000

    start login saga on page load

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import { TopBar, Footer, AppHeader, getTernMenu } from "tern-react";
 
 import { CONFIG } from "./config";
+import { checkLoginStatusStartAction } from "./store/reducer";
 import BioImagesAppHeader from "./components/headers/BioImagesAppHeader";
 import BioImagesEngine from "./components/BioImagesEngine";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(checkLoginStatusStartAction());
+  });
+
   return (
     <Router>
       <TopBar menuConfig={getTernMenu(CONFIG.MENU)} />
