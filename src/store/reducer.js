3350ddf638030d0a82d75a719c3ca3c7af3c061c
Gerhard Weis <g.weis@uq.edu.au>
2020-09-17 08:28:41 +1000

    add login state to redux store and reducers

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -145,7 +145,27 @@ const uiReducer = createReducer(initialUiState, {
   },
 });
 
+export const checkLoginStatusAction = createAction("CHECK_LOGIN_STATUS");
+export const checkLoginStatusStartAction = createAction("CHECK_LOGIN_STATUS_START");
+export const checkLoginStatusStopAction = createAction("CHECK_LOGIN_STATUS_STOP");
+export const checkLoginStatusSuccessAction = createAction("CHECK_LOGIN_STATUS_SUCCESS");
+
+const initialLoginState = {
+  checking: false,
+  user: null,
+};
+
+const loginReducer = createReducer(initialLoginState, {
+  [checkLoginStatusAction]: (state, action) => {
+    state.checking = action.payload;
+  },
+  [checkLoginStatusSuccessAction]: (state, action) => {
+    state.user = action.payload;
+  },
+});
+
 export const rootReducer = combineReducers({
   search: searchReducer,
   ui: uiReducer,
+  login: loginReducer,
 });
