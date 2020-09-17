32ee7e663c67ba7d82c54921097ea2c8ecc8db78
Gerhard Weis <g.weis@uq.edu.au>
2020-09-17 13:17:50 +1000

    simplify disabled styling on pagination

--- src/components/bio-search/search-control-bar/BioPagination.js
+++ src/components/bio-search/search-control-bar/BioPagination.js
@@ -14,9 +14,14 @@ import "./BioPagination.scss";
 // define a debounced dispatch to fire search only once while user is typing
 // this method needs to be defined outside of component, otherwise the debounced
 // method would be recreated on each state change which would defeat the purpose
+// TODO: think about bringing this back
+//       -> if yes, need to fix editing single digits (may onFoucs select all?
+//       .. still need backspace to empty though)
+//       -> noe way might be to use a custom input field with internal state?
 const debouncedDispatch = debounce((dispatch, action) => {
   dispatch(action);
 }, 1000);
+
 const BioPagination = () => {
   const dispatch = useDispatch();
   // ui data out of store
@@ -28,6 +33,10 @@ const BioPagination = () => {
   const totalDocuments = useSelector((state) => state.search.totalDocuments) || 0;
 
   const pages = Math.ceil(totalDocuments / page_size);
+
+  const updatePagination = (data) => {
+    dispatch(updateFilterAction({ pagination: data }));
+  }
 
   // change Page to given page, if delay is true,
   // it will debounce the fetchSearchAction dispatch
@@ -51,9 +60,7 @@ const BioPagination = () => {
       return;
     }
     // first update state
-    dispatch(
-      updateFilterAction({ pagination: { page_size, page_num: newPage } }),
-    );
+    updatePagination({ page_size, page_num: newPage });
     // trigger search
     if (delay) {
       debouncedDispatch(dispatch, fetchSearchAction());
@@ -65,90 +72,63 @@ const BioPagination = () => {
   };
 
   return (
-    <div className="pagination">
-      <Pagination
-        size="sm"
-        // style={{ paddingBottom: "6px" }}
+    <Pagination size="sm">
+      <PaginationItem
+        disabled={page_num === "" || parseInt(page_num, 10) === 1}
       >
-        <PaginationItem
-          className={
-            page_num === "" || parseInt(page_num, 10) === 1
-              ? "disable-pagination"
-              : "page-item"
-          }
-          disabled={page_num === "" || parseInt(page_num, 10) === 1}
-        >
-          <PaginationLink first title="First" onClick={() => changePage(1)} />
-        </PaginationItem>
-        <PaginationItem
-          className={
-            page_num === "" || parseInt(page_num, 10) === 1
-              ? "disable-pagination"
-              : "page-item"
-          }
-          disabled={page_num === "" || parseInt(page_num, 10) === 1}
-        >
-          <PaginationLink
-            previous
-            title="Previous"
-            onClick={() => changePage(page_num - 1)}
+        <PaginationLink first title="First" onClick={() => changePage(1)} />
+      </PaginationItem>
+      <PaginationItem
+        disabled={page_num === "" || parseInt(page_num, 10) === 1}
+      >
+        <PaginationLink
+          previous
+          title="Previous"
+          onClick={() => changePage(page_num - 1)}
+        />
+      </PaginationItem>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          changePage(Number(page_num), true);
+        }}
+      >
+        <div className="page-input">
+          <Input
+            title="Enter page number"
+            size="4"
+            min="1"
+            max={pages}
+            type="number"
+            bsSize="sm"
+            value={page_num}
+            onChange={(e) => {
+              dispatch(
+                updateFilterAction({
+                  pagination: {
+                    page_num: e.currentTarget.value.replace(/\D/, ""),
+                  },
+                }),
+              );
+            }}
           />
-        </PaginationItem>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            changePage(Number(page_num), true);
-          }}
-        >
-          <div className="page-input">
-            <Input
-              title="Enter page number"
-              size="4"
-              // min="1"
-              pattern="[0-9]*"
-              max={pages}
-              type="text"
-              bsSize="sm"
-              value={page_num}
-              onChange={(e) => {
-                dispatch(
-                  updateFilterAction({
-                    pagination: {
-                      page_size,
-                      page_num: e.currentTarget.value.replace(/\D/, ""),
-                    },
-                  }),
-                );
-              }}
-            />
-          </div>
-        </form>
-        <PaginationItem
-          className={
-            page_num === "" || parseInt(page_num, 10) === pages
-              ? "disable-pagination"
-              : "page-item"
-          }
-          disabled={page_num === "" || parseInt(page_num, 10) === pages}
-        >
-          <PaginationLink
-            next
-            title="Next"
-            onClick={() => changePage(Number(page_num) + 1)}
-          />
-        </PaginationItem>
-        <PaginationItem
-          className={
-            page_num === "" || parseInt(page_num, 10) === pages
-              ? "disable-pagination"
-              : "page-item"
-          }
-          disabled={page_num === "" || parseInt(page_num, 10) === pages}
-        >
-          <PaginationLink last title="Last" onClick={() => changePage(pages)} />
-        </PaginationItem>
-      </Pagination>
-    </div>
+        </div>
+      </form>
+      <PaginationItem
+        disabled={page_num === "" || parseInt(page_num, 10) === pages}
+      >
+        <PaginationLink
+          next
+          title="Next"
+          onClick={() => changePage(Number(page_num) + 1)}
+        />
+      </PaginationItem>
+      <PaginationItem
+        disabled={page_num === "" || parseInt(page_num, 10) === pages}
+      >
+        <PaginationLink last title="Last" onClick={() => changePage(pages)} />
+      </PaginationItem>
+    </Pagination>
   );
 };
 
