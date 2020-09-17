43c7bd86a615a2e17d5c7673d41657e38aa9393a
Gerhard Weis <g.weis@uq.edu.au>
2020-09-17 20:19:10 +1000

    logout url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export const CONFIG = {
   API_BASE_URL: "https://bioimages-test.tern.org.au/api",
   LOGIN_URL: "https://bioimages-test.tern.org.au/api/oidc/login",
+  LOGOUT_URL: "https://bioimages-test.tern.org.au/api/oidc/logout",
   MENU: {
     env: "test",
   },
