b6ef657e3d2b609c6417798f3c2462d968a6d4cd
Gerhard Weis <g.weis@uq.edu.au>
2020-09-29 06:41:51 +1000

    pull state and action handling into BioSearchControlBar simplify search control bar components (no state or action handling)

--- src/components/bio-search/search-control-bar/BioPagination.js
+++ src/components/bio-search/search-control-bar/BioPagination.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import {
   Pagination,
   PaginationItem,
@@ -6,9 +7,7 @@ import {
   Input,
   Form,
 } from "reactstrap";
-import { useSelector, useDispatch } from "react-redux";
-import debounce from "lodash/debounce";
-import { updateFilterAction, fetchSearchAction } from "../../../store/reducer";
+// import debounce from "lodash/debounce";
 
 import "./BioPagination.scss";
 
@@ -19,123 +18,131 @@ import "./BioPagination.scss";
 //       -> if yes, need to fix editing single digits (may onFoucs select all?
 //       .. still need backspace to empty though)
 //       -> noe way might be to use a custom input field with internal state?
-const debouncedDispatch = debounce((dispatch, action) => {
-  dispatch(action);
-}, 1000);
+// const debouncedDispatch = debounce((dispatch, action) => {
+//   dispatch(action);
+// }, 1000);
 
-const BioPagination = () => {
-  const dispatch = useDispatch();
-  // ui data out of store
-  const { page_size, page_num } = useSelector(
-    (state) => state.ui.searchFilters.pagination,
-  );
+const BioPagination = ({ pageNum, pages, onChange }) => {
+  // // change Page to given page, if delay is true,
+  // // it will debounce the fetchSearchAction dispatch
+  // const changePage = (page, delay = false) => {
+  //   // TODO: maybe move value checks into reducer?
+  //   let newPage = Number(page);
+  //   if (Number.isNaN(newPage)) {
+  //     // input doesn't make any sense
+  //     // cancel actions in case there are any
+  //     debouncedDispatch.cancel();
+  //     return;
+  //   }
+  //   if (delay && page === "") {
+  //     // still typing just update store, so that current input is reflected in input field
+  //     setPageNum(page, false);
+  //     // cancel actions in case there are any
+  //     debouncedDispatch.cancel();
+  //     return;
+  //   }
+  //   // some sanitiy checks on new page number
+  //   // don't go below 1
+  //   if (newPage < 1) {
+  //     newPage = 1;
+  //   }
+  //   // don't go beyond max pages
+  //   if (newPage > pages) {
+  //     newPage = pages;
+  //   }
+  //   // did it change at all?
+  //   if (newPage === page_num) {
+  //     // no change ... we either have an update queued in debounce
+  //     // or nothing needs to happen anyway
+  //     if (!delay) {
+  //       // don't wait, flush debounced events
+  //       debouncedDispatch.flush();
+  //     }
+  //     // otherwise do nothing
+  //     return;
+  //   }
+  //   // trigger search
+  //   if (delay) {
+  //     // TODO: update url needs to happen before we dispatch fetchSearch
+  //     setPageNum(newPage);
+  //     debouncedDispatch(dispatch, fetchSearchAction({
+  //       queryString: history.location.search,
+  //     }));
+  //   } else {
+  //     // cancel any debounced dispatchs
+  //     debouncedDispatch.cancel();
+  //     setPageNum(newPage);
+  //     dispatch(fetchSearchAction({
+  //       queryString: history.location.search,
+  //     }));
+  //   }
+  // };
 
-  // result data out of store
-  const totalDocuments = useSelector((state) => state.search.totalDocuments) || 0;
-
-  const pages = Math.ceil(totalDocuments / page_size);
-
-  const updatePagination = (data) => {
-    dispatch(updateFilterAction({ pagination: data }));
+  const handleInput = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    onChange(event.target.elements.page_num.value);
   };
 
-  // change Page to given page, if delay is true,
-  // it will debounce the fetchSearchAction dispatch
-  const changePage = (page, delay = false) => {
-    // TODO: maybe move value checks into reducer?
-    let newPage = Number(page);
-    if (Number.isNaN(newPage)) {
-      // input doesn't make any sense
-      // cancel actions in case there are any
-      debouncedDispatch.cancel();
-      return;
-    }
-    if (delay && page === "") {
-      // still typing just update store, so that current input is reflected in input field
-      updatePagination({ page_size, page_num: page });
-      // cancel actions in case there are any
-      debouncedDispatch.cancel();
-      return;
-    }
-    // some sanitiy checks on new page number
-    // don't go below 1
-    if (newPage < 1) {
-      newPage = 1;
-    }
-    // don't go beyond max pages
-    if (newPage > pages) {
-      newPage = pages;
-    }
-    // did it change at all?
-    if (newPage === page_num) {
-      // no change ... we either have an update queued in debounce
-      // or nothing needs to happen anyway
-      if (!delay) {
-        // don't wait, flush debounced events
-        debouncedDispatch.flush();
-      }
-      // otherwise do nothing
-      return;
-    }
-    // first update store
-    updatePagination({ page_size, page_num: newPage });
-    // trigger search
-    if (delay) {
-      debouncedDispatch(dispatch, fetchSearchAction());
-    } else {
-      // cancel any debounced dispatchs
-      debouncedDispatch.cancel();
-      dispatch(fetchSearchAction());
+  const handlePage = (nextPage) => {
+    if (nextPage !== pageNum && nextPage >= 1 && nextPage <= pages) {
+      onChange(nextPage);
     }
   };
 
   return (
     <Pagination size="sm">
-      <PaginationItem disabled={page_num === 1}>
-        <PaginationLink first title="First" onClick={() => changePage(1)} />
+      <PaginationItem disabled={pageNum <= 1}>
+        <PaginationLink first title="First" onClick={() => handlePage(1)} />
       </PaginationItem>
-      <PaginationItem disabled={page_num === 1}>
+      <PaginationItem disabled={pageNum <= 1}>
         <PaginationLink
           previous
           title="Previous"
-          onClick={() => changePage(page_num - 1)}
+          onClick={() => handlePage(pageNum - 1)}
         />
       </PaginationItem>
       <PaginationItem>
         <Form
           inline
           className="page-input"
-          onSubmit={(e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            changePage(page_num);
-          }}
+          onSubmit={handleInput}
         >
           <Input
             title="Enter page number"
+            name="page_num"
             size="4"
             min="1"
             max={pages}
             type="number"
             bsSize="sm"
-            value={page_num}
-            onBlur={() => changePage(page_num)}
-            onChange={(e) => { changePage(e.currentTarget.value, true); }}
+            // force re init on prop change
+            key={pageNum}
+            defaultValue={pageNum}
+            onBlur={(e) => e.target.form.requestSubmit()}
+          // TODO: onChange: could do debounced subnit?
+          // onChange={(e) => { changePage(e.currentTarget.value, true); }}
           />
         </Form>
       </PaginationItem>
-      <PaginationItem disabled={page_num === pages}>
+      <PaginationItem disabled={pageNum >= pages}>
         <PaginationLink
           next
           title="Next"
-          onClick={() => changePage(page_num + 1)}
+          onClick={() => handlePage(pageNum + 1)}
         />
       </PaginationItem>
-      <PaginationItem disabled={page_num === pages}>
-        <PaginationLink last title="Last" onClick={() => changePage(pages)} />
+      <PaginationItem disabled={pageNum >= pages}>
+        <PaginationLink last title="Last" onClick={() => handlePage(pages)} />
       </PaginationItem>
     </Pagination>
   );
+};
+
+BioPagination.propTypes = {
+  pageNum: PropTypes.number.isRequired,
+  pages: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default BioPagination;
--- src/components/bio-search/search-control-bar/BioSortOrder.js
+++ src/components/bio-search/search-control-bar/BioSortOrder.js
@@ -1,54 +1,41 @@
 import React from "react";
+import PropTypes from "prop-types";
 import {
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
 } from "reactstrap";
-import { useSelector, useDispatch } from "react-redux";
-import { updateFilterAction, fetchSearchAction } from "../../../store/reducer";
-import { bioSort } from "./bioSort";
 import "./BioSortOrder.scss";
 
-const BioSortOrder = () => {
-  const dispatch = useDispatch();
-  const { sort_order, sort_column } = useSelector(
-    (state) => state.ui.searchFilters.sort,
-  );
-  const selectedSortOrder = bioSort.sort_order.filter(
-    (sort) => sort.sort_name === sort_order,
-  );
+const BioSortOrder = ({ sortOrder, values, onChange }) => (
+  <div
+    className="mobile-pagination"
+  >
+    <UncontrolledDropdown className="sort-order-item" size="sm">
+      Sort Order:
+      {" "}
+      <DropdownToggle size="sm" caret color="sort-order-item" className="sort-order-item">
+        {sortOrder.label}
+      </DropdownToggle>
+      <DropdownMenu>
+        {values.map((sort) => (
+          <DropdownItem
+            key={sort.value}
+            onClick={() => onChange(sort)}
+          >
+            {sort.label}
+          </DropdownItem>
+        ))}
+      </DropdownMenu>
+    </UncontrolledDropdown>
+  </div>
+);
 
-  const handleSortOrder = (value) => {
-    dispatch(
-      updateFilterAction({ sort: { sort_order: value, sort_column } }),
-    );
-    dispatch(fetchSearchAction());
-  };
-
-  return (
-    <div
-      className="mobile-pagination"
-    >
-      <UncontrolledDropdown className="sort-order-item" size="sm">
-        Sort Order:
-        {" "}
-        <DropdownToggle size="sm" caret color="sort-order-item" className="sort-order-item">
-          {selectedSortOrder[0].sort_label}
-        </DropdownToggle>
-        <DropdownMenu>
-          {bioSort.sort_order.map((sort) => (
-            <DropdownItem
-              key={sort.sort_name}
-              onClick={() => handleSortOrder(sort.sort_name)}
-            >
-              {sort.sort_label}
-            </DropdownItem>
-          ))}
-        </DropdownMenu>
-      </UncontrolledDropdown>
-    </div>
-  );
+BioSortOrder.propTypes = {
+  sortOrder: PropTypes.objectOf(PropTypes.any).isRequired,
+  values: PropTypes.arrayOf(PropTypes.any).isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default BioSortOrder;
